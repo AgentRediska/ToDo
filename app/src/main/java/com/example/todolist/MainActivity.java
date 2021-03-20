2acb3a8abc3606d1a0f142cc6c411e1498ae1b43
AgentRediska <deniahalk98@mail.ru>
2021-03-20 13:35:27 +0300

    MainFragment implements the MVP structure

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,47 +3,17 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.example.todolist.contract.MainContract;
-import com.example.todolist.presenter.MainPresenter;
 
-public class MainActivity extends AppCompatActivity implements MainContract.View {
+public class MainActivity extends AppCompatActivity {
 
-    private MainContract.Presenter mPresenter;
-
-    private Button mButton;
-
-    private TextView myTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPresenter =new MainPresenter(this);
-
-        myTv=(TextView)findViewById(R.id.textView);
-        mButton=(Button)findViewById(R.id.button);
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mPresenter.onButtonAddEvent();
-            }
-        });
     }
 
-    @Override
-    public void showText(String message) {
-        myTv.setText(message);
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mPresenter.onDestroy();
-    }
 }
---
+++ app/src/main/java/com/example/todolist/view_fragment/MainFragment.java
@@ -0,0 +1,59 @@
+package com.example.todolist.view_fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.todolist.R;
+import com.example.todolist.contract.MainContract;
+import com.example.todolist.presenter.MainPresenter;
+
+
+public class MainFragment extends Fragment implements MainContract.View {
+
+  private MainContract.Presenter mPresenter;
+
+  private Button mButton;
+  private TextView mTextView;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPresenter= new MainPresenter(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_main, container, false);
+        mTextView=(TextView)view.findViewById(R.id.text2);
+        mButton=(Button)view.findViewById(R.id.button2);
+        mButton.setOnClickListener(v -> {
+            mPresenter.onButtonAddEvent();
+        });
+        return view;
+    }
+
+    @Override
+    public void showText(String message) {
+        mTextView.setText(message);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mPresenter.onDestroy();
+    }
+}
