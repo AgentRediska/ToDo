5a06acb5b7e42932bca539b153006b5fa972df23
AgentRediska <deniahalk98@mail.ru>
2021-05-17 23:19:33 +0300

    дизайнерские корректировки

--- app/src/main/java/com/example/todolist/recycler_view/DailyTasksViewHolderDone.java
+++ app/src/main/java/com/example/todolist/recycler_view/DailyTasksViewHolderDone.java
@@ -2,6 +2,7 @@ package com.example.todolist.recycler_view;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
